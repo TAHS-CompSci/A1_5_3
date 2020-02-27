2d0b4795d79e676f5fb5377c0a521afcbdb0617d
NK3216 <57916851+NK3216@users.noreply.github.com>
2020-02-27 12:38:59 -0600

    work

--- 1_5_3_PY_StudentSourceFiles/1.5.3.PY radius_changer.py
+++ 1_5_3_PY_StudentSourceFiles/1.5.3.PY radius_changer.py
@@ -16,25 +16,25 @@ root = tkinter.Tk()
 #####
 # Create Model
 ######
-radius_intvar = tkinter.IntVar()
-radius_intvar.set(100) #initialize radius
+y_intvar = tkinter.IntVar()
+y_intvar.set(100) #initialize radius
 # center of circle
 x = 150 
-y = 150
+r = 50
 
 ######
 # Create Controller
 #######
 # Event handler for slider
-def radius_changed(new_intval):
+def y_changed(new_intval):
     # Get data from model
     # Could do this: r = int(new_intval)
-    r = radius_intvar.get()
+    y = y_intvar.get()
     # Controller updating the view
     canvas.coords(circle_item, x-r, y-r, x+r, y+r)
 # Instantiate and place slider
-radius_slider = tkinter.Scale(root, from_=1, to=150, variable=radius_intvar,    
-                              label='Radius', command=radius_changed)
+radius_slider = tkinter.Scale(root, from_=1, to=300, variable=y_intvar,    
+                              label='Y', command=y_changed)
 radius_slider.grid(row=1, column=0, sticky=tkinter.W)
 # Create and place directions for the user
 text = tkinter.Label(root, text='Drag slider \nto adjust\ncircle.')
@@ -48,7 +48,7 @@ canvas = tkinter.Canvas(root, width=300, height=300, background='#FFFFFF')
 canvas.grid(row=0, rowspan=2, column=1)
 
 # Create a circle on the canvas to match the initial model
-r = radius_intvar.get()
+y = y_intvar.get()
 circle_item = canvas.create_oval(x-r, y-r, x+r, y+r, 
                                  outline='#000000', fill='#00FFFF')
 #######
