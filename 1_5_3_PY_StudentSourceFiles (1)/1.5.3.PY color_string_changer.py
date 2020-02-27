1a3ea3822565316e0d73dc3db51aab8eb8808c94
th3340 <th3340@k12.sd.us>
2020-02-27 13:20:28 -0600

    no

--- 1_5_3_PY_StudentSourceFiles (1)/1.5.3.PY color_string_changer.py
+++ 1_5_3_PY_StudentSourceFiles (1)/1.5.3.PY color_string_changer.py
@@ -3,25 +3,25 @@
 # 
 # Creates two Scales and a Text view. Updates Text based on the Scales
 # 
-# (c) 2013 PLTW
+# (c) 2013 PLtW
 # version 11/1/2013
 ####
 
-import Tkinter # Often people import Tkinter as *
+import tkinter # Often people import tkinter as *
 
 #####
 # Create root window 
 ####
-root = Tkinter.Tk()
+root = tkinter.Tk()
 root.wm_title('Hexadecimal Explorer')
 
 #####
 # Create Model
 ######
 # Create two IntVar's and initialize them to 127
-red_intvar = Tkinter.IntVar() 
+red_intvar = tkinter.IntVar() 
 red_intvar.set(127) 
-green_intvar = Tkinter.IntVar()
+green_intvar = tkinter.IntVar()
 green_intvar.set(127)
 
 ######
@@ -30,29 +30,29 @@ green_intvar.set(127)
 # Event handler for slider
 def color_changed(new_intval):
     # Controller updates the view by pulling data from model
-    editor.insert(Tkinter.END, '#' + \
+    editor.insert(tkinter.END, '#' + \
                                hexstring(red_intvar) + \
                                hexstring(green_intvar) + '00\n')
-    editor.see(Tkinter.END) # scroll the Text window to see the new bottom line
+    editor.see(tkinter.END) # scroll the Text window to see the new bottom line
             
 # Instantiate and place sliders
-red_slider = Tkinter.Scale(root, from_=0, to=255, variable=red_intvar, 
-                           orient=Tkinter.HORIZONTAL,   
+red_slider = tkinter.Scale(root, from_=0, to=255, variable=red_intvar, 
+                           orient=tkinter.HORIZONTAL,   
                            label='Red', command=color_changed)
-red_slider.grid(row=1, column=0, sticky=Tkinter.E)
-green_slider = Tkinter.Scale(root, from_=0, to=255, variable=green_intvar,  
-                             orient=Tkinter.HORIZONTAL,
+red_slider.grid(row=1, column=0, sticky=tkinter.E)
+green_slider = tkinter.Scale(root, from_=0, to=255, variable=green_intvar,  
+                             orient=tkinter.HORIZONTAL,
                              label='Green', command=color_changed)
-green_slider.grid(row=2, column=0, sticky=Tkinter.E)
+green_slider.grid(row=2, column=0, sticky=tkinter.E)
 # Create and place directions for the user
-text = Tkinter.Label(root, text='Drag slider \nto adjust\ncolor code.')
-text.grid(row=0, column=0)
+Text = tkinter.Label(root, Text='Drag slider \nto adjust\ncolor code.')
+Text.grid(row=0, column=0)
 
 ######
 # Create View
 #######
-# Create a text editor window for displaying information
-editor = Tkinter.Text(root, width=10)
+# Create a Text editor window for displaying information
+editor = tkinter.Text(root, width=10)
 editor.grid(column=1, row=0, rowspan=3)
 
 ######
--- 1_5_3_PY_StudentSourceFiles (1)/1.5.3.PY radius_changer.py
+++ 1_5_3_PY_StudentSourceFiles (1)/1.5.3.PY radius_changer.py
@@ -2,11 +2,11 @@
 # radius_changer.py
 # 
 # Creates a Scale and a Canvas. Updates a circle based on the Scale.
-# (c) 2013 PLTW
+# (c) 2013 PLtW
 # version 11/1/2013
 ####
 
-import tkinter #often people import Tkinter as *
+import tkinter #often people import tkinter as *
 
 #####
 # Create root window 
@@ -16,11 +16,11 @@ root = tkinter.Tk()
 #####
 # Create Model
 ######
-radius_intvar = tkinter.IntVar()
-radius_intvar.set(100) #initialize radius
+y_intvar = tkinter.IntVar()
+y_intvar.set(100) #initialize radius
 # center of circle
 x = 150 
-r = 150
+r = 50
 
 ######
 # Create Controller
@@ -33,8 +33,8 @@ def y_changed(new_intval):
     # Controller updating the view
     canvas.coords(circle_item, x-r, y-r, x+r, y+r)
 # Instantiate and place slider
-radius_slider = tkinter.Scale(root, from_=1, to=150, variable=radius_intvar,    
-                              label='Radius', command=y_changed)
+radius_slider = tkinter.Scale(root, from_=1, to=300, variable=y_intvar,    
+                              label='y', command=y_changed)
 radius_slider.grid(row=1, column=0, sticky=tkinter.W)
 # Create and place directions for the user
 text = tkinter.Label(root, text='Drag slider \nto adjust\ncircle.')
@@ -48,7 +48,7 @@ canvas = tkinter.Canvas(root, width=300, height=300, background='#FFFFFF')
 canvas.grid(row=0, rowspan=2, column=1)
 
 # Create a circle on the canvas to match the initial model
-r = radius_intvar.get()
+y = y_intvar.get()
 circle_item = canvas.create_oval(x-r, y-r, x+r, y+r, 
                                  outline='#000000', fill='#00FFFF')
 #######
