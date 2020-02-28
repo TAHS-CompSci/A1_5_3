2f891d99ff9890a9e31719bd0174cc67a4ac5b8d
Drayzdin <57916828+Dt3202@users.noreply.github.com>
2020-02-28 13:09:18 -0600

    work

--- MVCpattern.py
+++ MVCpattern.py
@@ -89,11 +89,13 @@ def down(event):
 
 
 def up(event):
+    xp = x_intvar.get()
+    yp = y_intvar.get()
     tk_color_string = color(red_intvar, green_intvar, blue_intvar)
     r = (startx-event.x)**2 + (starty-event.y)**2  
     r = int(r**.5)
     new_shape = canvas.create_rectangle(
-        x-r, y-r, x+r, y+r, outline=tk_color_string)
+        x-r+xp, y-r+yp, x+r+xp, y+r+yp, outline=tk_color_string)
     shapes.append(new_shape)  
 
 canvas.bind('<Button-1>', down)
